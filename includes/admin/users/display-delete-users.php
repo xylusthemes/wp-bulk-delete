87a468a39ff0d13f2ebcc7f9936079caa4810cfa
Rajat Patel <prajat21@gmail.com>
2024-09-02 16:44:59 +0530

    Plugin's new Interface

--- includes/admin/users/display-delete-users.php
+++ includes/admin/users/display-delete-users.php
@@ -21,50 +21,171 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @return void
  */
 function wpbd_delete_users_page(){
-
-	if(  ! empty( $_POST ) && isset( $_POST['_delete_users_wpnonce'] ) ){
-	    // Get user_result for delete based on user input.
-	    $user_result = xt_delete_users_form_process( $_POST );
-	    wpbd_display_admin_notice( $user_result );
-	}	
 	?>
-	<div class="wrap">
-		<h2><?php esc_html_e('Delete Users','wp-bulk-delete'); ?></h2>
-		<div id="poststuff">
-			<div id="post-body" class="metabox-holder columns-2">
+	<div class="wpbd-container">
+		<div class="wpbd-wrap" >
+			<div id="poststuff">
+				<div id="post-body" class="metabox-holder columns-2">
+					<div id="postbox-container-2" class="postbox-container">
+						<form method="post" id="delete_users_form">
+							<div class="form-table">
+							
+								<div class="wpbd-card" >
+									<div class="header toggles" >
+										<div class="text" >
+											<div class="header-icon" ></div>
+											<div class="header-title" >
+												<span><?php _e('Users Filter ','wp-bulk-delete'); ?></span>
+											</div>
+											<div class="header-extra" ></div>
+										</div>
+										<svg viewBox="0 0 24 24" fill="none"
+											xmlns="http://www.w3.org/2000/svg" class="wpbd-caret rotated">
+											<path d="M16.59 8.29492L12 12.8749L7.41 8.29492L6 9.70492L12 15.7049L18 9.70492L16.59 8.29492Z" fill="currentColor"></path>
+										</svg>
+									</div>
+									<div class="content"  aria-expanded="true" style="">
+										<?php 
+											do_action( 'wpbd_delete_users_form' );
+										?>
+									</div>
+								</div>
 
-				<div class="notice notice-warning">
-					<p><strong><?php _e( 'WARNING: Before you delete any users please first take Backup, any delete operation done is irreversible. Please use it with caution!', 'wp-bulk-delete' ); ?></strong></p>
-				</div>
-				<?php do_action( 'timeout_memory_is_enough' ); ?>
+								<div class="wpbd-card" style="margin-top:10px;">
+									<div class="header toggles" >
+										<div class="text" >
+											<div class="header-icon" ></div>
+											<div class="header-title" >
+												<span><?php _e('Date Filter ','wp-bulk-delete'); ?></span>
+											</div>
+											<div class="header-extra" ></div>
+										</div>
+										<svg viewBox="0 0 24 24" fill="none"
+											xmlns="http://www.w3.org/2000/svg" class="wpbd-caret">
+											<path d="M16.59 8.29492L12 12.8749L7.41 8.29492L6 9.70492L12 15.7049L18 9.70492L16.59 8.29492Z" fill="currentColor"></path>
+										</svg>
+									</div>
+									<div class="content"  aria-expanded="false" style="display:none;">
+										<?php 
+											do_action( 'wpbd_delete_users_date_form' );
+										?>
+									</div>
+								</div>
 
-				<div class="delete_notice"></div>
+								<div class="wpbd-card" style="margin-top:10px;">
+									<div class="header toggles" >
+										<div class="text" >
+											<div class="header-icon" ></div>
+											<div class="header-title" >
+												<span><?php _e('Advance Users Filter ','wp-bulk-delete'); if( !wpbd_is_pro() ){ echo '<div class="wpbd-pro-badge"> PRO </div>'; } ?></span>
+											</div>
+											<div class="header-extra" ></div>
+										</div>
+										<svg viewBox="0 0 24 24" fill="none"
+											xmlns="http://www.w3.org/2000/svg" class="wpbd-caret">
+											<path d="M16.59 8.29492L12 12.8749L7.41 8.29492L6 9.70492L12 15.7049L18 9.70492L16.59 8.29492Z" fill="currentColor"></path>
+										</svg>
+									</div>
+									<div class="content"  aria-expanded="false" style="display:none;">
+										<?php 
+											if( !wpbd_is_pro() ){
+												?>
+												<div class="wpbd-blur-filter" >
+													<div class="wpbd-blur" >
+														<div class="wpbd-blur-filter-option">
+															<?php
+																wpbd_render_form_custom_fields();
+																wpbd_render_form_post_contains();
+															?>
+														</div>
+													</div>
+													<div class="wpbd-blur-filter-cta" >
+													<span style="color: red"><?php echo _e( 'Available in Pro version.', 'wp-bulk-delete' ); ?>  </span><a href="<?php echo esc_url(WPBD_PLUGIN_BUY_NOW_URL); ?>"><?php echo _e( 'Buy Now', 'wp-bulk-delete' ); ?></a>
+													</div>
+												</div>
+												<?php
+											}else{
+												do_action( 'wpbd_delete_users_advance_form' );
+											}
+										?>
+									</div>
+								</div>
 
-				<div id="postbox-container-1" class="postbox-container">
-					<?php do_action('wpbd_admin_sidebar'); ?>
-				</div>
+								<div class="wpbd-card" style="margin-top:10px;">
+									<div class="header toggles" >
+										<div class="text" >
+											<div class="header-icon" ></div>
+											<div class="header-title" >
+												<span><?php _e('WooCommerce Filter ','wp-bulk-delete'); if( !wpbd_is_pro() ){ echo '<div class="wpbd-pro-badge"> PRO </div>'; } ?></span>
+											</div>
+											<div class="header-extra" ></div>
+										</div>
+										<svg viewBox="0 0 24 24" fill="none"
+											xmlns="http://www.w3.org/2000/svg" class="wpbd-caret">
+											<path d="M16.59 8.29492L12 12.8749L7.41 8.29492L6 9.70492L12 15.7049L18 9.70492L16.59 8.29492Z" fill="currentColor"></path>
+										</svg>
+									</div>
+									<div class="content"  aria-expanded="false" style="display:none;">
+										<?php 
+											if( !wpbd_is_pro() ){
+												?>
+												<div class="wpbd-blur-filter" >
+													<div class="wpbd-blur" >
+														<div class="wpbd-blur-filter-option">
+															<?php
+																do_action( 'wpbd_delete_users_woocommerce_form' );
+															?>
+														</div>
+													</div>
+													<div class="wpbd-blur-filter-cta" >
+													<span style="color: red"><?php echo _e( 'Available in Pro version.', 'wp-bulk-delete' ); ?>  </span><a href="<?php echo esc_url(WPBD_PLUGIN_BUY_NOW_URL); ?>"><?php echo _e( 'Buy Now', 'wp-bulk-delete' ); ?></a>
+													</div>
+												</div>
+												<?php
+											}else{
+												do_action( 'wpbd_delete_users_woocommerce_form' );
+											}
+										?>
+									</div>
+								</div>
 
-				<div id="postbox-container-2" class="postbox-container">
-					<form method="post" id="delete_users_form">
-    					<table class="form-table">
+								<div class="wpbd-card" style="margin-top:10px;">
+									<div class="header toggles" >
+										<div class="text" >
+											<div class="header-icon" ></div>
+											<div class="header-title" >
+												<span><?php _e('Action ','wp-bulk-delete'); ?></span>
+											</div>
+											<div class="header-extra" ></div>
+										</div>
+										<svg viewBox="0 0 24 24" fill="none"
+											xmlns="http://www.w3.org/2000/svg" class="wpbd-caret rotated">
+											<path d="M16.59 8.29492L12 12.8749L7.41 8.29492L6 9.70492L12 15.7049L18 9.70492L16.59 8.29492Z" fill="currentColor"></path>
+										</svg>
+									</div>
+									<div class="content"  aria-expanded="true" style="">
+										<?php 
+											do_action( 'wpbd_delete_users_action_limit_form' );
+											wpbd_render_delete_time();
+										?>
+									</div>
+								</div>
+
+							</div>
 							<?php
-							do_action( 'wpbd_delete_users_form' );
-							wpbd_render_delete_time();
-    						?>
-    					</table>
-    					<?php
-    					echo wp_nonce_field('delete_users_nonce', '_delete_users_wpnonce' );
-    					?>
-    					<p class="submit">
-					        <input name="delete_users_submit" id="delete_users_submit" class="button button-primary" value="<?php esc_html_e('Delete Users', 'wp-bulk-delete');?>" type="button">
-					        <span class="spinner" style="float: none;"></span>
-					    </p>
-    				</form>
+							echo wp_nonce_field('delete_users_nonce', '_delete_users_wpnonce' );
+							?>
+							<p class="submit">
+								<input type="hidden" name="action" value="wpbd_delete_post">
+								<input name="delete_users_submit" id="delete_users_submit" class="wpbd_button" value="<?php esc_html_e('Delete Users', 'wp-bulk-delete');?>" type="button">
+								<span class="spinner" style="float: none;"></span>
+							</p>
+						</form>
+					</div>
 				</div>
+				<br class="clear">
 			</div>
-			<br class="clear">
-		</div>
-
-	</div><!-- /.wrap -->
+		</div><!-- /.wrap -->
+	</div>
 	<?php
 }
--- includes/delele-terms-form-functions.php
+++ includes/delele-terms-form-functions.php
@@ -74,18 +74,28 @@ function xt_delete_terms_form_process( $data ) {
  * @return void
  */
 function wpbd_render_delete_terms_taxonomy(){?>
-    <tr>
-        <th scope="row">
-            <?php _e('Post Taxonomy:','wp-bulk-delete'); ?>
-        </th>
-        <td>
+    <div class="wpbd-inner-main-section">
+        <div class="wpbd-inner-section-1" >
+            <span class="wpbd-title-text" >
+                <?php _e('Post Taxonomy ','wp-bulk-delete'); ?>
+                <span class="wpbd-tooltip" >
+                    <div>
+                        <svg viewBox="0 0 20 20" fill="#000" xmlns="http://www.w3.org/2000/svg" class="wpbd-circle-question-mark">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.6665 10.0001C1.6665 5.40008 5.39984 1.66675 9.99984 1.66675C14.5998 1.66675 18.3332 5.40008 18.3332 10.0001C18.3332 14.6001 14.5998 18.3334 9.99984 18.3334C5.39984 18.3334 1.6665 14.6001 1.6665 10.0001ZM10.8332 13.3334V15.0001H9.1665V13.3334H10.8332ZM9.99984 16.6667C6.32484 16.6667 3.33317 13.6751 3.33317 10.0001C3.33317 6.32508 6.32484 3.33341 9.99984 3.33341C13.6748 3.33341 16.6665 6.32508 16.6665 10.0001C16.6665 13.6751 13.6748 16.6667 9.99984 16.6667ZM6.6665 8.33341C6.6665 6.49175 8.15817 5.00008 9.99984 5.00008C11.8415 5.00008 13.3332 6.49175 13.3332 8.33341C13.3332 9.40251 12.6748 9.97785 12.0338 10.538C11.4257 11.0695 10.8332 11.5873 10.8332 12.5001H9.1665C9.1665 10.9824 9.9516 10.3806 10.6419 9.85148C11.1834 9.43642 11.6665 9.06609 11.6665 8.33341C11.6665 7.41675 10.9165 6.66675 9.99984 6.66675C9.08317 6.66675 8.33317 7.41675 8.33317 8.33341H6.6665Z" fill="currentColor"></path>
+                        </svg>
+                        <span class="wpbd-popper">
+                            <?php _e( 'Select the post taxonomy whose terms you want to delete.', 'wp-bulk-delete' ); ?>
+                            <div class="wpbd-popper__arrow"></div>
+                        </span>
+                    </div>
+                </span>
+            </span>
+        </div>
+        <div class="wpbd-inner-section-2">
             <div class="post_taxonomy">
             </div>
-            <p class="description">
-                <?php _e( 'Select the post taxonomy whose terms you want to delete.', 'wp-bulk-delete' ); ?>
-            </p>
-        </td>
-    </tr>
+        </div>
+    </div>
     <script>
         jQuery(document).ready(function(){
             jQuery('#delete_post_type').trigger( 'change' );
